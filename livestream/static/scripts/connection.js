4b6c3ec764d01dfd3d53ff4e51d1f51c77bbc407
RenanPinhoAssi <renanpinhoassi@gmail.com>
2020-11-12 01:53:04 -0300

    Update connection.js

--- livestream/static/scripts/connection.js
+++ livestream/static/scripts/connection.js
@@ -17,34 +17,34 @@ var initialize_connection = function () {
 
   // PROBLEM
 
-  SOCKET.on("call-made", async (data) => {
-    console.log("call-made (making answer) " + data.socket);
-    console.log(all_peers[data.socket])
-    var getPeer = all_peers[data.socket];
-    await getPeer["peerConnection"].setRemoteDescription(
-      new RTCSessionDescription(data.offer)
-    );
-    var answer = await getPeer["peerConnection"].createAnswer();
-    await getPeer["peerConnection"].setLocalDescription(
-      new RTCSessionDescription(answer)
-    );
+//   SOCKET.on("call-made", async (data) => {
+//     console.log("call-made (making answer) " + data.socket);
+//     console.log(all_peers[data.socket])
+//     var getPeer = all_peers[data.socket];
+//     await getPeer["peerConnection"].setRemoteDescription(
+//       new RTCSessionDescription(data.offer)
+//     );
+//     var answer = await getPeer["peerConnection"].createAnswer();
+//     await getPeer["peerConnection"].setLocalDescription(
+//       new RTCSessionDescription(answer)
+//     );
 
-    console.log("CALL-MADE IS NOT DEAD")
-    console.log(getPeer["peerConnection"]) 
-    SOCKET.emit("make-answer", {
-      answer,
-      to: data.socket,
-    });
-  });
+//     console.log("CALL-MADE IS NOT DEAD")
+//     console.log(getPeer["peerConnection"]) 
+//     SOCKET.emit("make-answer", {
+//       answer,
+//       to: data.socket,
+//     });
+//   });
 
-  SOCKET.on("answer-made", async (data) => {
-    console.log("answer-made (call-user) " + data.socket);
-    var getPeer = all_peers[data.socket];
-    await getPeer["peerConnection"].setRemoteDescription(
-      new RTCSessionDescription(data.answer)
-    );
-    console.log("ANSWER-MADE IS NOT DEAD")
-    console.log(getPeer["peerConnection"])
-    callUser(data.socket);
-  });
-};
+//   SOCKET.on("answer-made", async (data) => {
+//     console.log("answer-made (call-user) " + data.socket);
+//     var getPeer = all_peers[data.socket];
+//     await getPeer["peerConnection"].setRemoteDescription(
+//       new RTCSessionDescription(data.answer)
+//     );
+//     console.log("ANSWER-MADE IS NOT DEAD")
+//     console.log(getPeer["peerConnection"])
+//     callUser(data.socket);
+//   });
+// };
