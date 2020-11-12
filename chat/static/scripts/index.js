c9a7773de0ac0c1389d04b2f1d85f96818c46ef6
RenanPinhoAssi <renanpinhoassi@gmail.com>
2020-11-12 01:16:36 -0300

    More Trouble

--- chat/static/scripts/index.js
+++ chat/static/scripts/index.js
@@ -5,70 +5,71 @@
 var print = function(msg){
   document.getElementById("demo-text").innerHTML += "<br/>"+msg;
 }
-print("ATLEAST JAVASCRIPT IS WORKING")
-// var HTML_skip_Encode = function (encoded_string) {
-//     let result = { object: null, filtered_message: null };
-//     let element = null;
-//     try {
-//       element = document.createElement("p");
-//       element.innerHTML = element.textContent = encoded_string;
-//       result["object"] = element;
-//       result["filtered_message"] = encoded_string;
-//     } catch (e) {}
+print("ATLEAST JAVASCRIPT IS WORKING");
+
+var HTML_skip_Encode = function (encoded_string) {
+    let result = { object: null, filtered_message: null };
+    let element = null;
+    try {
+      element = document.createElement("p");
+      element.innerHTML = element.textContent = encoded_string;
+      result["object"] = element;
+      result["filtered_message"] = encoded_string;
+    } catch (e) {}
   
-//     return result;
-//   };
+    return result;
+  };
 
-// var HTML_Encode = function (not_encoded_string) {
-//   let result = { object: null, filtered_message: null };
-//   let element = null;
-//   try {
-//     let encoded_string = not_encoded_string.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-//         return '&#'+i.charCodeAt(0)+';';
-//      });
-//     element = document.createElement("p");
-//     element.innerHTML = element.textContent = encoded_string;
-//     result["object"] = element;
-//     result["filtered_message"] = encoded_string;
-//   } catch (e) {}
+var HTML_Encode = function (not_encoded_string) {
+  let result = { object: null, filtered_message: null };
+  let element = null;
+  try {
+    let encoded_string = not_encoded_string.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+        return '&#'+i.charCodeAt(0)+';';
+     });
+    element = document.createElement("p");
+    element.innerHTML = element.textContent = encoded_string;
+    result["object"] = element;
+    result["filtered_message"] = encoded_string;
+  } catch (e) {}
 
-//   return result;
-// };
+  return result;
+};
 
-// var send_message_flow = function () {
-//   let filter_message = HTML_Encode($("#text_field").val());
-//   let message = filter_message["filtered_message"];
-//   let p_element = filter_message["object"];
-//   if (message !== "") {
-//     let li_element = document.createElement("li");
-//     li_element.setAttribute("class", "own_message");
-//     li_element.appendChild(p_element);
-//     message_panel.appendChild(li_element);
-//     $("#text_field").val("");
+var send_message_flow = function () {
+  let filter_message = HTML_Encode($("#text_field").val());
+  let message = filter_message["filtered_message"];
+  let p_element = filter_message["object"];
+  if (message !== "") {
+    let li_element = document.createElement("li");
+    li_element.setAttribute("class", "own_message");
+    li_element.appendChild(p_element);
+    message_panel.appendChild(li_element);
+    $("#text_field").val("");
 
-//     message_holder.scrollTop = message_holder.scrollHeight;
+    message_holder.scrollTop = message_holder.scrollHeight;
 
-//     print("ALMOST SENDING MESSAGE");
-//     socket.emit("send_message", message);
-//     print("MESSAGE SENT!");
-//   }
-// };
+    print("ALMOST SENDING MESSAGE");
+    socket.emit("send_message", message);
+    print("MESSAGE SENT!");
+  }
+};
 
-// var receive_message_flow = function (message) {
-//   let filter_message = HTML_skip_Encode(message);
-//   message = filter_message["filtered_message"];
-//   let p_element = filter_message["object"];
-//   if (message !== "") {
-//     let li_element = document.createElement("li");
-//     li_element.setAttribute("class", "other_message");
-//     li_element.appendChild(p_element);
-//     message_panel.appendChild(li_element);
+var receive_message_flow = function (message) {
+  let filter_message = HTML_skip_Encode(message);
+  message = filter_message["filtered_message"];
+  let p_element = filter_message["object"];
+  if (message !== "") {
+    let li_element = document.createElement("li");
+    li_element.setAttribute("class", "other_message");
+    li_element.appendChild(p_element);
+    message_panel.appendChild(li_element);
 
-//     message_holder.scrollTop = message_holder.scrollHeight;
-//   }
-// };
+    message_holder.scrollTop = message_holder.scrollHeight;
+  }
+};
 
-// print("I'M JAVASCRIPT BITCH!");
+print("I'M JAVASCRIPT BITCH!");
 
 // $(function () {
 //   socket = io();
