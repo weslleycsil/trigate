4bfffe445e99a7887d739cc69c10bfa91efc6ffe
RenanPinhoAssi <renanpinhoassi@gmail.com>
2020-11-25 13:53:42 -0300

    Update server.js

--- RTCMultiConnection-master/server.js
+++ RTCMultiConnection-master/server.js
@@ -226,39 +226,11 @@ if (isUseHTTPs) {
     // See how to use a valid certificate:
     // https://github.com/muaz-khan/WebRTC-Experiment/issues/62
     var options = {
-        key: key,
-        cert: cert,
+        key: fs.readFileSync("./key.pem"),
+        cert: fs.readFileSync("./cert.pem"),
         ca: null
     };
 
-    var pfx = false;
-
-    if (!fs.existsSync(config.sslKey)) {
-        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslKey:\t ' + config.sslKey + ' does not exist.');
-    } else {
-        pfx = config.sslKey.indexOf('.pfx') !== -1;
-        options.key = key;
-    }
-
-    if (!fs.existsSync(config.sslCert)) {
-        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCert:\t ' + config.sslCert + ' does not exist.');
-    } else {
-        options.cert = cert;
-    }
-
-    if (config.sslCabundle) {
-        if (!fs.existsSync(config.sslCabundle)) {
-            console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCabundle:\t ' + config.sslCabundle + ' does not exist.');
-        }
-
-        options.ca = fs.readFileSync(config.sslCabundle);
-    }
-
-    if (pfx === true) {
-        options = {
-            pfx: sslKey
-        };
-    }
 
     httpApp = httpServer.createServer(options, serverHandler);
 } else {
