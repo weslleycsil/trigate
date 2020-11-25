67c81f18df7f79a294fe3a82995c3759c8502880
RenanPinhoAssi <renanpinhoassi@gmail.com>
2020-11-25 13:05:51 -0300

    Update server.js

--- RTCMultiConnection-master/server.js
+++ RTCMultiConnection-master/server.js
@@ -6,7 +6,7 @@ var httpServer = require('http');
 const ioServer = require('socket.io');
 const RTCMultiConnectionServer = require('rtcmulticonnection-server');
 
-var PORT = 9001;
+var PORT = 8080;
 var isUseHTTPs = false;
 
 const jsonPath = {
