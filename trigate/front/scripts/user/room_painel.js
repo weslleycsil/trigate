1c4a11739f288f81af15af5944a2dfe65ebaf3e5
RenanPinhoAssi <renanpinhoassi@gmail.com>
2020-11-27 15:03:22 -0300

    Change Style

--- trigate/front/scripts/user/room_painel.js
+++ trigate/front/scripts/user/room_painel.js
@@ -11,7 +11,7 @@ var SCREENS = [
 var CURRENT_SCREEN = 0;
 
 var populate_room_painel = function () {
-  $("#room-name").text("Sala" + get_optional_attr("room-display-name"))
+  $("#room-name").text("Sala " + get_optional_attr("room-display-name"))
   $("#broadcast-frame-holder").attr("src", "https://g3ws.dev.br:8080?room-id=" + get_optional_attr("room-id"));
   $(SCREENS[(CURRENT_SCREEN + 1) % 2]["object"]).hide();
   $("#button-toggle-screen").text(SCREENS[CURRENT_SCREEN]["name"]);
