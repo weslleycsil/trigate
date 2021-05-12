5161da6a88e9711c66a1928d64925f0fe62c0cf2
Renan Pinho Assi <renanpinhoassi@gmail.com>
2021-05-12 17:14:26 -0300

    update

--- trigate/front/scripts/user/room_painel.js
+++ trigate/front/scripts/user/room_painel.js
@@ -12,7 +12,7 @@ var CURRENT_SCREEN = 0;
 
 var populate_room_painel = function () {
   $("#room-name").text("Sala " + get_optional_attr("room-display-name"))
-  $("#broadcast-frame-holder").attr("src", "https://g3ws.dev.br:8080?room-id=" + get_optional_attr("room-id"));
+  $("#broadcast-frame-holder").attr("src", "https://ufsc3d.inf.ufsc.br:3000?room-id=" + get_optional_attr("room-id"));
   $(SCREENS[(CURRENT_SCREEN + 1) % 2]["object"]).hide();
   $("#button-toggle-screen").text(SCREENS[CURRENT_SCREEN]["name"]);
 
