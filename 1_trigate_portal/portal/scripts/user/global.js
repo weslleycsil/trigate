112ab1bf36563159344ffd2272709e702adc94bc
Weslley Silva <weslleycsil@gmail.com>
2022-07-23 16:53:51 -0300

    acertos de url

--- 1_trigate_portal/portal/scripts/user/global.js
+++ 1_trigate_portal/portal/scripts/user/global.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 
-var base_url = "https://trigate.com";
+var base_url = "https://ufsc3d.inf.ufsc.br/developer1_5.2";
 var api_base_url = base_url + "/api";
 var gallery_base_url = base_url + "/gallery/";
 
